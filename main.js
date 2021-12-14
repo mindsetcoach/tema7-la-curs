c667d65a7f7fe8115c7e7a7192a83b11f6698750
Stefan L <lazustefan@gmail.com>
2021-12-14 21:32:59 +0200

    tema 7 final

--- main.js
+++ main.js
@@ -2,18 +2,25 @@ let userInput = prompt("Enter your number!");
 
 let parentArray = [];
 let userNumber = parseInt(userInput);
+let mainDivElement = document.getElementById('mainDiv');
 
 for ( let i=0 ; i<userNumber ; i= i+1 ) {
     let childArray = [];
-
-
+    let lineDiv = document.createElement("div");
+    mainDivElement.appendChild(lineDiv);
     for ( let j=0 ; j<userNumber ; j= j+1 ) {
         let theRandomNumber = Math.floor(Math.random() * 9) + 1;
         childArray.push(theRandomNumber);
         let childDiv = document.createElement("div");
-        childDiv.innerHTML = theRandomNumber;
-        let mainDivElement = document.getElementById('mainDiv');
-        mainDivElement.appendChild(childDiv);
+        childDiv.classList.add("child");
+        if ( theRandomNumber%2 !== 0) {
+            childDiv.innerHTML = theRandomNumber;
+        }
+        else {
+            childDiv.innerHTML = "0";
+            childDiv.style.color = "white";
+        }
+        lineDiv.appendChild(childDiv);
     }
     parentArray.push(childArray);
 }
