ab3f03d44595063906bafb1a4428b385847497f7
Tudor Goia <mindsetcoach360@gmail.com>
2021-12-14 20:00:07 +0200

    small fine tuning

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ let userNumber = parseInt(userInput);
 for ( let i=0 ; i<userNumber ; i= i+1 ) {
     let childArray = [];
     parentArray.push(childArray);
-    var theRandomNumber = Math.floor(Math.random() * 10) + 1;
+    let theRandomNumber = Math.floor(Math.random() * 9) + 1;
 }
 
