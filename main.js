25b42c9293acfc9ff9cda8219f1b366ecccf0a4d
Ionut Falos <ionut_falos@yahoo.it>
2021-12-14 20:42:37 +0200

    ionut

--- main.js
+++ main.js
@@ -5,7 +5,15 @@ let userNumber = parseInt(userInput);
 
 for ( let i=0 ; i<userNumber ; i= i+1 ) {
     let childArray = [];
-    parentArray.push(childArray);
-    let theRandomNumber = Math.floor(Math.random() * 9) + 1;
-}
 
+
+    for ( let j=0 ; j<userNumber ; j= j+1 ) {
+        let theRandomNumber = Math.floor(Math.random() * 9) + 1;
+        childArray.push(theRandomNumber);
+        let childDiv = document.createElement("div");
+        childDiv.innerHTML = theRandomNumber;
+        let mainDivElement = document.getElementById('mainDiv');
+        mainDivElement.appendChild(childDiv);
+    }
+    parentArray.push(childArray);
+}
