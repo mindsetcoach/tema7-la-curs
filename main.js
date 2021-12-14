9674c5cd65591f0a6aeff6c9b57f64a4a67df422
Mercean Vlad <merceanv@gmail.com>
2021-12-14 19:51:26 +0200

    firstSteps

---
+++ main.js
@@ -0,0 +1,11 @@
+let userInput = prompt("Enter your number!");
+
+let parentArray = [];
+let userNumber = parseInt(userInput);
+
+for ( let i=0 ; i<userNumber ; i= i+1 ) {
+    let childArray = [];
+    parentArray.push(childArray);
+    var theRandomNumber = Math.floor(Math.random() * 10) + 1;
+}
+
